a686f3be4cd5ae08115f03c3e5507b7f33ebf8a2
Suleyman Baris Eser <suleymanbariseser@gmail.com>
2023-04-10 23:32:04 +0300

    improve example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -19,36 +19,50 @@ const Box: React.FC<React.PropsWithChildren> = ({ children }) => {
   );
 };
 
-const App = () => {
-  const [likeVariant, setLikeVariantValue] = useVariant('like');
-  const [testVariant, setTestVariantValue] = useVariant('test');
+const LikeVariant = () => {
+  const [value, setValue] = useVariant('like');
 
   return (
+    <Box>
+      <button onClick={() => setValue('A')}>A</button>
+      <button onClick={() => setValue('B')}>B</button>
+      <Experiment name="like" defaultValue="A">
+        <Variant name="A">
+          <p>User has variant A for Likes</p>
+        </Variant>
+        <Variant name="B">
+          <p>User has variant B for Likes</p>
+        </Variant>
+      </Experiment>
+    </Box>
+  );
+};
+
+const TestVariant = () => {
+  const [value, setValue] = useVariant('test');
+
+  return (
+    <Box>
+      <button onClick={() => setValue('A')}>A</button>
+      <button onClick={() => setValue('B')}>B</button>
+      <Experiment name="test" defaultValue="B">
+        <Variant name="A">
+          <p>User has variant A for Tests</p>
+        </Variant>
+        <Variant name="B">
+          <p>User has variant B for Tests</p>
+        </Variant>
+      </Experiment>
+    </Box>
+  );
+};
+
+const App = () => {
+  return (
     <div>
-      <Box>
-        <button onClick={() => setLikeVariantValue('A')}>A</button>
-        <button onClick={() => setLikeVariantValue('B')}>B</button>
-        <Experiment name="like" defaultValue="A">
-          <Variant name="A">
-            <p>User has variant A for Likes</p>
-          </Variant>
-          <Variant name="B">
-            <p>User has variant B for Likes</p>
-          </Variant>
-        </Experiment>
-      </Box>
-      <Box>
-        <button onClick={() => setTestVariantValue('A')}>A</button>
-        <button onClick={() => setTestVariantValue('B')}>B</button>
-        <Experiment name="test" defaultValue="B">
-          <Variant name="A">
-            <p>User has variant A for Tests</p>
-          </Variant>
-          <Variant name="B">
-            <p>User has variant B for Tests</p>
-          </Variant>
-        </Experiment>
-      </Box>
+      <LikeVariant />
+      <TestVariant />
+      <LikeVariant />
     </div>
   );
 };
